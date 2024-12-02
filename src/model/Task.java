f6b93fd3d0caea8e9236e309cb02d5f07b7a4d1a
daniyardoskulov <daniyar.doskulov@narxoz.kz>
2024-12-02 19:01:20 +0500

    "sprint8 fix style"

--- src/model/Task.java
+++ src/model/Task.java
@@ -21,7 +21,7 @@ public class Task {
         this.duration = duration;
     }
 
-    public Task(int id,String name,String description){
+    public Task(int id,String name,String description) {
         this.id = id;
         this.name = name;
         this.description = description;
